a28ff8048d45362a2526b05d0f90fdf6e190f533
Pheerawit Wasinphongwanit <pheerawit.was@gec.co.th>
2017-09-03 11:40:49 +0700

    feat: implement the components to render from mocked data
    I mocked the data for using to render in Shelves and BookGrids
    components. And implement the render() functions.

--- src/Shelves.js
+++ src/Shelves.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
+import './Shelves.css';
+import BookGrids from './components/BookGrids'
 
+/**
+ * @description: Represents all book shelves
+ * @constructor
+ */
 class Shelves extends Component {
     render() {
+        const {data} = this.props;
+
         return (
             <div>
-                <h1>Shelves!!</h1>
+                {
+                    data.map(shelf=>
+                        <div className="bookshelf" key={shelf.sorId}>
+                            <h2 className="bookshelf-title">shelf.category</h2>
+                            <div className="bookshelf-books">
+                                <BookGrids books={shelf.books}/>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
         );
     }
