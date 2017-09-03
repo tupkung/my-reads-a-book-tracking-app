d2c84ddd9ad896ce945d08361a91cb70203adc0b
Pheerawit Wasinphongwanit <pheerawit.was@gec.co.th>
2017-09-03 10:40:15 +0700

    feat: Add new Component Shelves for showing all book shelves
    I thought to have a component which control to display the book shelves
    in eache categories. So, I create the new component Shelves to receive
    "data" property and generate all shelves into it.

---
+++ src/Shelves.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Shelves extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Shelves!!</h1>
+            </div>
+        );
+    }
+}
+
+export default Shelves;
